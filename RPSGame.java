9b459944284cf86e857286d63236198b514f3bad
Justin Appave <justin.appave@ucalgary.ca>
2017-01-23 12:40:54 -0700

    Update RPSGame.java

--- RPSGame.java
+++ RPSGame.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class RPSGame{
   public static void main(String[] args){
+    Scanner keyboard = new Scanner(System.in);
+    System.out.println("Rock-Paper-Scissors!");
+    Syetem.out.print("Player 1, please make your choice: ");
+    String player1 = keyboard.next();
+    System.out.print("Player 2, make your choice: ");
+    String player 2 = keyboard.next();
+    
+    int rock = 1;
+    int paper = 2;
+    int scissors = 3;
+    
+    if(player1 == "r"){
+      int player1Choice = 1}
+    else if(player1 == "R"){
+      int player1Choice = 1}
+    
     
   }
 }
