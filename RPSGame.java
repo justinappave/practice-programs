1ad94dc872bd806c625e7043ddfeb57ad6ced91a
Justin Appave <justin.appave@ucalgary.ca>
2017-01-23 13:02:34 -0700

    Add files via upload

--- RPSGame.java
+++ RPSGame.java
@@ -4,20 +4,23 @@ public class RPSGame{
   public static void main(String[] args){
     Scanner keyboard = new Scanner(System.in);
     System.out.println("Rock-Paper-Scissors!");
-    Syetem.out.print("Player 1, please make your choice: ");
+    System.out.print("Player 1, please make your choice: ");
     String player1 = keyboard.next();
     System.out.print("Player 2, make your choice: ");
-    String player 2 = keyboard.next();
+    String player2 = keyboard.next();
     
     int rock = 1;
     int paper = 2;
     int scissors = 3;
     
-    if(player1 == "r"){
-      int player1Choice = 1}
-    else if(player1 == "R"){
-      int player1Choice = 1}
+    if(player1.equals("r") || player1.equals("R")){
+		int player1Choice = 1;
+		System.out.println(player1Choice);}
+	else if(player1.equals("p") || player1.equals("P")){
+		int player1Choice = 2;}
+	else if(player1.equals("s") || player1.equals("s")){
+		int player1Choice = 3;}
     
-    
+	System.out.println(player1Choice);
   }
 }
